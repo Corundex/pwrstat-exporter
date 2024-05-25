82b234fb6ad96acafb37e688230ee892ba8f24b7
Anatoly Butko <anatoly@butko.net>
2024-05-25 14:38:34 -0400

    ALARM control and more logs

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
 
+echo ===========================================================================
+date
+echo ===========================================================================
 /etc/init.d/pwrstatd start
+sleep 1
+echo ===========================================================================
+
+if [ "$ALARM" = "on" ]; then
+    pwrstat -alarm on
+    echo "Alarm turned on."
+    elif [ "$ALARM" = "off" ]; then
+    pwrstat -alarm off
+    echo "Alarm turned off."
+fi
+
+pwrstat -config
+echo ===========================================================================
+pwrstat -status
+echo ===========================================================================
 /app/pwrstat-exporter
